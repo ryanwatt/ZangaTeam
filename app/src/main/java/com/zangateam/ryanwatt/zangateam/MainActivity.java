8b757159886fa1ac1c6cc01e9ac683972fc6d749
raleigh wayland <rtwayland@gmail.com>
2016-06-01 13:26:29 -0600

    something

--- app/src/main/java/com/zangateam/ryanwatt/zangateam/MainActivity.java
+++ app/src/main/java/com/zangateam/ryanwatt/zangateam/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void notATest() {
         String testString = "This IS a test"; //Raleigh's comment
+        // another comment
     }
 }
