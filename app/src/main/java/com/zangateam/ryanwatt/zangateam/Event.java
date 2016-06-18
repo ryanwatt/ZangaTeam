2d8794830dcbd704a9856240e972d8ce15395267
Nick <nicholas.zarate@gmail.com>
2016-06-18 10:59:29 -0600

    Add Event class to encapsulate a single Event that holds a title, time, and description

---
+++ app/src/main/java/com/zangateam/ryanwatt/zangateam/Event.java
@@ -0,0 +1,31 @@
+package com.zangateam.ryanwatt.zangateam;
+
+/**
+ * Created by nickzarate on 6/16/16.
+ */
+public class Event {
+    String description;
+    String time;
+    String title;
+    // Insert photo here
+
+    public String getDescription() {
+        return description;
+    }
+    public String getTime() {
+        return time;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
