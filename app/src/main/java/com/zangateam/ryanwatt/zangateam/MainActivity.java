01e9b7471e2b679bd8deb36fccc1b5222423e75a
raleigh wayland <rtwayland@gmail.com>
2016-06-21 12:51:01 -0600

    Pulling evenlist from Feed, then setting the adapter and adding it to ListView. Not working...

--- app/src/main/java/com/zangateam/ryanwatt/zangateam/MainActivity.java
+++ app/src/main/java/com/zangateam/ryanwatt/zangateam/MainActivity.java
@@ -1,12 +1,19 @@
 package com.zangateam.ryanwatt.zangateam;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Feed feed;
+    Context context;
+    ArrayAdapter<Event> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Feed feed = new Feed(this);
         feed.execute();
+
+        List<Event> events = feed.getEvents();
+        ListView listView = (ListView) findViewById(R.id.eventsList);
+        adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, events);
+        listView.setAdapter(adapter);
     }
 }
